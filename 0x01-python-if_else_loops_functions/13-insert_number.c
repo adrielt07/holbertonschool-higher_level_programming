26d7d0b673365f51722dbeb70044526ba30b2a74
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-11 18:19:15 +0000

    accounted for in head is null insert new node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,13 +11,16 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *current = *head, *prev, *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
+
+	if (head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
 	if (current->n > number)
 	{
