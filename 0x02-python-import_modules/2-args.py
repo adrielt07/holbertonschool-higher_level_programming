5e09e2a123d57d78461a90e43dc9998860ad513c
adrielt07 <adriel_tolentino@outlook.com>
2018-04-12 22:54:45 +0000

    added another if statement if 0 argv is passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     a = 0
-    if (len(sys.argv) == 2):
+    if (len(sys.argv) == 1):
+        print("{} arguments.".format(len(sys.argv)-1))
+    elif (len(sys.argv) == 2):
         print("{} argument:".format(len(sys.argv)-1))
         print("{}:".format(len(sys.argv)-1), sys.argv[1])
 
