de7ade45a3edd9e38ece7803a66d2e90753d836c
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-17 19:45:07 +0000

    Don't use str.join

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print("\n".join("{:d}".format(x) for x in my_list))
+    for x in my_list:
+        print("{:d}".format(x))
+
+#    print("\n".join("{:d}".format(x) for x in my_list))
