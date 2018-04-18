16ea97d5891f76496e724ee0aade1b4577306088
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-18 00:02:51 +0000

    remove condition if malloc failed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#define buffer 9999
 
 /**
  * len_list - gets the len of linked list
@@ -26,9 +25,11 @@ int len_list(listint_t *head)
 
 int is_palindrome(listint_t **head)
 {
-	int list[buffer];
-	int i = 0, len = len_list(*head), j = 0;
+	unsigned int *list;
+	int i = 0, len = len_list(*head) - 1, j = 0;
 	listint_t *current = *head;
+
+	list = malloc(sizeof(unsigned int *) * len);
 
 	while (current)
 	{
@@ -36,13 +37,17 @@ int is_palindrome(listint_t **head)
 		current = current->next;
 		i++;
 	}
-	len--;
+
 	while (len >= 0)
 	{
 		if (list[len] != list[j])
+		{
+			free(list);
 			return (0);
+		}
 		len--;
 		j++;
 	}
+	free(list);
 	return (1);
 }
