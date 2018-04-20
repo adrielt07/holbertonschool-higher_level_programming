1862ab02e58c22ee33131aa526d6490bb9f3475f
adrielt07 <adriel_tolentino@outlook.com>
2018-04-20 14:30:34 +0000

    first commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    mult = 0
+    ave = 0
+    for w, a in my_list:
+        mult += w * a
+        ave += a
+
+    result = mult / ave
+    return result
