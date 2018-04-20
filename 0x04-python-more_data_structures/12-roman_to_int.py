668385b7e98dea9924b0231ee8658ea5ec323cdf
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-20 01:55:48 +0000

    account for if roman nums are out of order excluding the last two roman num

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,15 +3,29 @@ def roman_to_int(roman_string):
 
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     r_string = list(roman_string)
+    r_string.reverse()
     result = 0
+    check = 0
+
     for char in r_string:
         if r_string.count(char) > 3:
             return None
 
     length = len(roman_string)
+    if length > 3:
+        for i in range(length-1):
+            if roman[roman_string[i]] < roman[roman_string[i+1]]:
+                check += 1
+    if check >= 1:
+        return None
     for idx in range(length):
         result += roman[roman_string[idx]]
     if len(roman_string) > 1:
         if roman_string[length-1] > roman_string[length-2]:
             result -= roman[roman_string[idx-1]]*2
     return result
+#    for elem in r_string:
+#        if r_string[0] > elem:
+#            check += 1
+#        if check > 1:
+#            return None
