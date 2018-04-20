6fe3140b80e232a2839d20477434ceb620fcc9da
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-20 04:03:29 +0000

    remove some of the edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,33 +8,16 @@ def roman_to_int(roman_string):
     if isinstance(roman_string, str) is False:
         return 0
 
-    r_string = list(roman_string)
-    r_string.reverse()
-    for char in r_string:
-        if r_string.count(char) > 3:
-            return 0
+#    r_string = list(roman_string)
+#    r_string.reverse()
+#    for char in r_string:
+#        if r_string.count(char) > 3:
+#            return 0
 
     length = len(roman_string)
     for idx in range(length):
         result += roman[roman_string[idx]]
     if len(roman_string) > 1:
         if roman[roman_string[-1]] > roman[roman_string[-2]]:
-            result -= roman[roman_string[idx-1]]*2
+            result -= roman[roman_string[-2]]*2
     return result
-
-# Some Edge cases:
-
-#    if length > 3:
-#        for i in range(length-1):
-#            if roman[roman_string[i]] < roman[roman_string[i+1]]:
-#                check += 1
-#    if check >= 1:
-#        return 0
-
-
-
-#    for elem in r_string:
-#        if r_string[0] > elem:
-#            check += 1
-#        if check > 1:
-#            return None
