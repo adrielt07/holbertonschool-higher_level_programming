3d28a8ab4882d98dcaa008a7c5258ac20886b536
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-01 02:59:06 +0000

    change condition to is true

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     num = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], int) == True:
+            if isinstance(my_list[i], int) is True:
                 print("{:d}".format(my_list[i]), end="")
                 num += 1
     except (TypeError, ValueError):
