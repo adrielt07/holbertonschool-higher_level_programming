c1925ec2da548d0050d4fad827478bb8eeba95ae
adrielt07 <adriel_tolentino@outlook.com>
2018-05-02 00:11:57 +0000

    first commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Square is a Class:
+defines a square by size
+"""
+
+
+class Square:
+    """
+    Create a private instance attribute size
+    size must be an int
+    size must can't be negative
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """
+    Calculates the area of the square
+    """
+    def area(self):
+        return self.__size * self.__size
