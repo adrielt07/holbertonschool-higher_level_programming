c7205d97dbcae25a681641930f43f979223d8114
adrielt07 <adriel_tolentino@outlook.com>
2018-05-02 00:07:38 +0000

    first commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Square is a Class:
+defines a square by size
+"""
+
+
+class Square:
+    """
+    Create a private instance attribute size
+    size must be an int
+    size must can't be negative
+    """
+    def __init__(self, size):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
