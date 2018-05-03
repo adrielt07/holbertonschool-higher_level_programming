18d58b11d2a12e64d976b8eaad900d51a7a03367
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-03 22:42:51 +0000

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+add_integer adds two integers
+
+Args:
+a - takes an int
+b - takes an int, default value is 98
+
+Return:
+Sum of a and b
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two int or float types only. Else, raise a Type Error
+    """
+    if isinstance(a, int) is False and isinstance(a, float) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, int) is False and isinstance(b, float) is False:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
