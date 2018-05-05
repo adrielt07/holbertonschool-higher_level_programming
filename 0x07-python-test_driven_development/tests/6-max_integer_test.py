4608a5d6a2a21827fb3d8291fd027960bbbc7de4
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-05 23:53:07 +0000

    added more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,19 +3,26 @@
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-my_list=[1, 2, 3, 4]
 
 class mytest(unittest.TestCase):
 
     def test_output(self):
-        self.assertAlmostEqual(max_integer(my_list), 4)
-        self.assertAlmostEqual(max_integer(my_list), 4)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 4, 3, 2]), 4)
+        self.assertAlmostEqual(max_integer([-1, -4, -3, -2]), -1)
+        self.assertAlmostEqual(max_integer([1]), 1)
         self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1, 2, 00]), 2)
 
-    def test_listtype(self):
-        self.assertNotIsInstance(max_integer(my_list), list, msg="Error")
+    def test_list(self):
+        self.assertIs(list, list)
 
-    def test_numtype(self):
-        self.assertRaises(TypeError, max_integer, "str")
-        self.assertRaises(TypeError, max_integer, 3+5j)
-        self.assertRaises(TypeError, max_integer, True)
+    def test_none(self):
+        self.assertRaises(TypeError, max_integer, None,
+                          msg="object of type 'NoneType' has no len()")
+
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer, [1, 2, "str"],
+                          msg="unorderable types: str() > int()")
+        self.assertRaises(TypeError, max_integer, [1, 2, 3+5j],
+                          msg="unorderable types: str() > complex()")
