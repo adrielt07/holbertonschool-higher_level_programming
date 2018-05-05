6409791d1f72b1f43b9e8e28a0f8a39077ea67c5
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-05 22:54:56 +0000

    first commit

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+my_list=[1, 2, 3, 4]
+
+class mytest(unittest.TestCase):
+
+    def test_output(self):
+        self.assertAlmostEqual(max_integer(my_list), 4)
+        self.assertAlmostEqual(max_integer(my_list), 4)
+
+    def test_listtype(self):
+        self.assertNotIsInstance(max_integer(my_list), list, msg="Error")
+
+    def test_numtype(self):
+        self.assertRaises(TypeError, max_integer, "str")
+        self.assertRaises(TypeError, max_integer, 3+5j)
+        self.assertRaises(TypeError, max_integer, True)
