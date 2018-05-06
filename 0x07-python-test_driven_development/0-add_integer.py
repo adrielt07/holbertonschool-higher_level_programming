faa0f8032b055c894411f06df8bdb1ca967de0e4
adrielt07 <adriel_tolentino@outlook.com>
2018-05-06 14:15:00 +0000

    change if statement to or

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,8 @@ def add_integer(a, b=98):
     """
     Adds two int or float types only. Else, raise a Type Error
     """
-    if isinstance(a, int) is False and isinstance(a, float) is False:
+    if isinstance(a, int) is False or isinstance(a, float) is False:
         raise TypeError("a must be an integer")
-    if isinstance(b, int) is False and isinstance(b, float) is False:
+    if isinstance(b, int) is False or isinstance(b, float) is False:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
