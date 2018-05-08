e156a99bdc1e7ecf610280be2fd0c03198df894d
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-08 01:20:00 +0000

    removed the if statements in __init__ setter since it's unnecessary Setter can take care of it

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -24,16 +24,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ Initializing variables width and height
         """
-        if isinstance(width, int) is False:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-
-        if isinstance(height, int) is False:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
