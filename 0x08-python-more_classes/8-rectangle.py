52d8e045be01c510e1ca0a5acc2ffe11514ea16a
adrielt07 <adriel_tolentino@outlook.com>
2018-05-08 19:44:26 +0000

    fix the type for type error

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -33,9 +33,9 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        if isinstance(height, int) is False:
+        if isinstance(width, int) is False:
             raise TypeError("width must be an integer")
-        if height < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
         self.__height = height
