0569b065c3cb48084a1353861ac31e80e31da641
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-15 19:59:24 +0000

    first commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Importing from 7-base_geometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class - Inhereting from BaseGeometery"""
+    def __init__(self, width, height):
+        """Checking if width or height is int greater than 0"""
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
