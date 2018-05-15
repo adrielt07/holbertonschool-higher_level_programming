6ab0d2711b05eb46e191baef2b355e7f31db408f
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-15 20:37:44 +0000

    fix pep8

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """returns string format"""
         return "[{}] {}/{}".format(type(self).__name__,
-                                 self.__width, self.__height)
+                                   self.__width, self.__height)
