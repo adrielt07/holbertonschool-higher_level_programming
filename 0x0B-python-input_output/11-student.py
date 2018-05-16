7f46474a12b0cfccaac174aea2a485a59a7014cb
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-16 22:28:28 +0000

    first commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module has one class:
+Student
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializing variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionaru representation of a student instance
+        """
+        return self.__dict__
