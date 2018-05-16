986e40553890cb2560777379d9c5d5402bd3279a
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-16 23:09:04 +0000

    first commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module has one class:
+Student
+"""
+
+
+class Student:
+    """Information that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializing variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a student instance
+        """
+        if attrs == None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            if isinstance(attrs, list) == True:
+                for elem in attrs:
+                    for key, val in self.__dict__.items():
+                        if elem == key:
+                            new_dict[elem] = val
+            return new_dict
