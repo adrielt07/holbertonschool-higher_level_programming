1bb880a99f817266084faf055ad572af163bc1d1
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-19 19:31:34 +0000

    added documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
+"""Module only has one Class: Base"""
 
 class Base:
+    """Class for Shapes Class to inherent from"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initialize instance variables
+
+        Purpose: Assigned an id for each instance
+        Usages:
+        b1 = base(12)
+        b1 will have id of 12 (b1.id)
+
+        if no id specified, id will automatically get assigned
+        starting from 1 and increments by 1 each call
+        b2 = base()
+        b2 will ahve id of 1 (b2.id)
+        b3 = base()
+        b3 will have id of 2 (b2.id)
+        """
         if id is not None:
             self.id = id
         else:
@@ -13,6 +30,7 @@ class Base:
     @staticmethod
     def checkattr(value, num):
         """
+        Validates the follow attributes
         1 = width
         2 = height
         3 = x
