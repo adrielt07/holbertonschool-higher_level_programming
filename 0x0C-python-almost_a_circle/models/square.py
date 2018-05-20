c8bf45efbf797da3ac74fdaebe46e4b3d4e6af94
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-20 04:00:56 +0000

    saving progress

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,14 +14,38 @@ class Square(Rectangle):
 
     def __str__(self):
         """print string"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.__size)
         return super().__str__(Square)
 
     @property
     def size(self):
         """returns size"""
-        return super().width
+        return self.__size
 
     @size.setter
     def size(self, value):
         super().checkattr(value, 1)
         self.__size = value
+
+    def update(self, *args, **kwargs):
+        """updates the attributes"""
+        if len(args) != 0:
+            if len(args) == 1:
+                self.id = args[0]
+            elif len(args) == 2:
+                self.id = args[0]
+                self.__size = args[1]
+            elif len(args) == 3:
+                self.id = args[0]
+                self.__size = args[1]
+                self.__x = args[2]
+            elif len(args) == 4:
+                self.id = args[0]
+                self.__size = args[1]
+                self.__x = args[2]
+                self.__y = args[3]
+
+        else:
+            for key, val in kwargs.items():
+                if hasattr(self, key) is True:
+                        setattr(self, key, val)
