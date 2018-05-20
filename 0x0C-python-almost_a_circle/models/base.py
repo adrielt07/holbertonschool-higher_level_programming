890ad2b2769039a797df4ac8a7a75187fc0971ee
adrielt07 <adriel_tolentino@outlook.com>
2018-05-20 05:32:45 +0000

    added staticmethod to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """Module only has one Class: Base"""
 
 class Base:
@@ -26,6 +27,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
     @staticmethod
     def checkattr(value, num):
