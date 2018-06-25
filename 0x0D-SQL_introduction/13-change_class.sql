4dea1a306058fe71b15cde718e5585955919116b
adrielt07 <adriel_tolentino@outlook.com>
2018-06-25 09:40:08 +0000

    Initial commit

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+DELETE from second_table where `score` <= 5;
