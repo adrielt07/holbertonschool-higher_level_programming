ec2fcbfc0cb462e16f254c75680d48fa39b5c89f
adrielt07 <adriel_tolentino@outlook.com>
2018-06-25 08:41:02 +0000

    account for if table already exists

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,4 +3,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
-CREATE TABLE first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
