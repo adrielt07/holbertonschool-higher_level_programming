1a20a2772bc0193d980b842b42b679664e6a89ba
adrielt07 <adriel_tolentino@outlook.com>
2018-06-25 10:20:16 +0000

    Fix the order of avg_temp to Descending

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- Import in hbtn_0c_0 database this table dump
 -- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
 -- Displays the average temperature by city ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ASC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY AVG(VALUE) DESC;
