dcce04308a62d53e57e79f98e58d053464b5f790
adrielt07 <adriel_tolentino@outlook.com>
2018-06-25 10:00:38 +0000

    Initial commit

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0
+-- Don’t list rows without a name value
+-- Results should display the score and the name (Descending score)
+-- The database name will be passed as an argument to the mysql command
+SELECT score, name FROM `second_table` WHERE name IS NOT NULL ORDER BY `score` DESC;
