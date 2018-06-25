f773c5f5cc3a2cd4656c1306e10194b4f0061769
adrielt07 <adriel_tolentino@outlook.com>
2018-06-25 09:28:02 +0000

    Initial commit

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name (highest score to lowest)
+-- The database name will be passed as an argument of the mysql command
+SELECT `score`, `name` FROM second_table WHERE `score` >= 10 ORDER BY `score` DESC;
