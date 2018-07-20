ff926c47fd75946f3a09d627a89994900c8ca34b
adrielt07 <adriel_tolentino@outlook.com>
2018-07-20 06:08:40 +0000

    pep8 compliant

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,9 +5,9 @@ instance Base = declarative_base():
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-
 Base = declarative_base()
+
+
 class State(Base):
     """
     This generates the State table with the following:
