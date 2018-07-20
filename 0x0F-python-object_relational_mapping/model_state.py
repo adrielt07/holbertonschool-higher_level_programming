69fe6e8ed470c546309e92a0197a144021816cec
adrielt07 <adriel_tolentino@outlook.com>
2018-07-20 06:06:44 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of a State and an
+instance Base = declarative_base():
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+class State(Base):
+    """
+    This generates the State table with the following:
+    Autogenerate an PK ID
+    Name of state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
