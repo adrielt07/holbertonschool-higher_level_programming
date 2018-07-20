be49f7b729786bc61e0119e395dede91e46dbeca
adrielt07 <adriel_tolentino@outlook.com>
2018-07-20 05:17:26 +0000

    use inner join for state column

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id")
 
     rows = cur.fetchall()
     for row in rows:
