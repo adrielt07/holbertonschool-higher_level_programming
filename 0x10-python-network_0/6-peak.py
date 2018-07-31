0960aad2bf4370455d4be118b0814b2865704f9a
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-31 22:13:35 +0000

    initial commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    finds a peak in the list of integers
+    """
+    if len(list_of_integers) == 0 or list_of_integers is None:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid = len(list_of_integers) / 2
+    mid = int(mid)
+    try:
+        if list_of_integers[mid - 1] < list_of_integers[mid] and list_of_integers[mid + 1] < list_of_integers[mid]:
+            return list_of_integers[mid]
+    except IndexError:
+        if list_of_integers[mid - 1] < list_of_integers[mid]:
+            return list_of_integers[mid]
+    if list_of_integers[mid - 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid:])
