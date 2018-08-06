c763f130470abe2c08fb7c3ef5fc5aa293506f5f
adrielt07 <adrieltolentino@outlook.com>
2018-08-06 22:45:15 +0000

    fix pep8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     try:
-        if r.raise_for_status() == None:
+        if r.raise_for_status() is None:
             print(r.text)
     except requests.exceptions.HTTPError as e:
         print("Error code: {}".format(r.status_code))
