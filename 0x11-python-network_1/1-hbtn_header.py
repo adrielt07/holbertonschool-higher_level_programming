74be01363d442dd3e2a2581d4d3bd0750ee27e0e
adrielt07 <adriel_tolentino@outlook.com>
2018-08-06 15:56:48 +0000

    used getheader attribute

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,4 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        header = response.info()
-    header = dict(header)
-    print(header['X-Request-Id'])
+        print(response.getheader('X-Request-Id'))
