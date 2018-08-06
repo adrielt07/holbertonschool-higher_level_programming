ac52ca833a930004daec21ffb5ec011e2c2e58b1
adrielt07 <adrieltolentino@outlook.com>
2018-08-06 22:43:49 +0000

    use try and except

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,8 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.raise_for_status() == None:
-        print(r.text)
-    else:
+    try:
+        if r.raise_for_status() == None:
+            print(r.text)
+    except requests.exceptions.HTTPError as e:
         print("Error code: {}".format(r.status_code))
