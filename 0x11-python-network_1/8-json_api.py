ec10203f8d0ce93c960684fe04e54eb7ecc3a7a0
adrielt07 <adrieltolentino@outlook.com>
2018-08-07 00:30:31 +0000

    initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3                                                                                       
+"""
+Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        value = {'q': sys.argv[1]}
+    except IndexError:
+        value = {'q': ""}
+    r = requests.post('http://0.0.0.0:5000/search_user', value)
+    res = r.json()
+    if len(res) == 0:
+        print("No result")
+    else:
+        print("[{}] {}".format(res['id'], res['name']))
