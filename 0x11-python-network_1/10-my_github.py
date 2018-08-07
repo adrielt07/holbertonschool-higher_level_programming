c7f365ff75d273246e63473b74a983824d27bf38
adrielt07 <adrieltolentino@outlook.com>
2018-08-07 01:14:55 +0000

    intial commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your Github credentials
+(username and password) and uses the Github API to display your id
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    r = r.json()
+    print(r['id'])
