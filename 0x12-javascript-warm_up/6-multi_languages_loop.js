df0f8f0ccb7fddaaddf7ebfeeb6942fe19d2f944
adrielt07 <adriel_tolentino@outlook.com>
2018-08-28 15:00:21 +0000

    initial commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+
+const array = [firstLine, secondLine, thirdLine];
+
+for (let i = 0; i < 3; i++) {
+  console.log(array[i]);
+}
