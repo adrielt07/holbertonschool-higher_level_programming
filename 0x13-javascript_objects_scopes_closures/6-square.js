247d518277305d4319e567791f02e33e934a7e98
adrielt07 <adrieltolentino@outlook.com>
2018-09-04 22:31:45 +0000

    removed contructor since it's unncessary

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,6 @@
 const ParentSquare = require('./5-square');
 
 module.exports = class Square extends ParentSquare {
-  // Inheriting from Square class from 5-square
-  constructor (size) {
-    super(size);
-  }
-
   // Method charPrint(c) - takes a Character that replaces 'X' when using print method
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
