80094c46a82d56554533c5fe2a9a175aab1dd254
adrielt07 <adriel_tolentino@outlook.com>
2018-09-05 02:39:21 +0000

    remove var usage and used let

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  var ret = 0;
-  var len = list.length;
+  let ret = 0;
+  let len = list.length;
   for (let index = 0; index < len; index++) {
     if (searchElement === list[index]) { ret++; }
   }
