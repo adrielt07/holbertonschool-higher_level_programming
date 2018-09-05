66c9925297cd719a4c78997e71b90115d63bb9de
adrielt07 <adrieltolentino@outlook.com>
2018-09-05 21:49:45 +0000

    initial commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let url = 'http://swapi.co/api/films/' + process.argv[2].toString();
+
+request(url, function (error, response, body) {
+  console.log(JSON.parse(body).title);
+});
