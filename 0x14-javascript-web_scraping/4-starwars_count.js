665c082d8df490df288c4841a6ae574e5d213336
adrielt07 <adrieltolentino@outlook.com>
2018-09-06 16:37:39 +0000

    revert changes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-let url;
 let Wedge = 'https://swapi.co/api/people/18/';
 let count = 0;
-
-if (url === undefined) {
-  url = 'http://swapi.co/api/films';
-} else {
-  url = process.argv[2];
-}
+let url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
