17306caf440dbcc864d2a58eb32ce34173575c8f
adrielt07 <adrieltolentino@outlook.com>
2018-09-07 22:14:51 +0000

    initial commit

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $.getJSON
+  (
+    'https://swapi.co/api/films/?format=json',
+    function (data, status) {
+      $.each(data.results, function (index, val) {
+        $('#list_movies').append('<li>' + val.title + '</li>');
+      });
+    }
+  );
+});
