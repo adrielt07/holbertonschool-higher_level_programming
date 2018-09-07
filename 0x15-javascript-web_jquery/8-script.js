37da46e2072cd7f938ca8064c6efeaa7dab550b9
adrielt07 <adrieltolentino@outlook.com>
2018-09-07 22:27:21 +0000

    fix semistandard

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,7 @@
 $(function () {
-  $.getJSON
-  (
-    'https://swapi.co/api/films/?format=json',
-    function (data, status) {
-      $.each(data.results, function (index, val) {
-        $('#list_movies').append('<li>' + val.title + '</li>');
-      });
-    }
-  );
+  $.getJSON('https://swapi.co/api/films/?format=json', function (data, status) {
+    $.each(data.results, function (index, val) {
+      $('#list_movies').append('<li>' + val.title + '</li>');
+    });
+  });
 });
